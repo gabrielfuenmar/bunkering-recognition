a37edddea7b6999d60a586b94a08ee9852d5bd22
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2020-06-07 20:43:38 +0200

    Update bunker_mpi.sh

--- bunker_mpi.sh
+++ bunker_mpi.sh
@@ -5,4 +5,4 @@
 #$ -j y
 #$ -S /bin/bash
 
-mpirun --mca btl vader,self,tcp  --map-by node -n 306 ~/anaconda3/envs/bunker2/bin/python /home/gabriel/codes/bunkering_all_med/barge_alongside_ship_all_med_cluster_new_DBS.py
+mpirun --mca btl vader,self,tcp  --map-by node -n 306 ~python barge_alongside_ship_all_med_cluster_new_DBS.py
